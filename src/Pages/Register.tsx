b15165b52cc79694ab0f529cac765ebc56fbfce6
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-08-06 11:20:47 +0100

    Update: Register page

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -11,58 +11,62 @@ const Register = () => {
     email:'',
     password:''
   })
+
+  const [progress, setProgress] = useState({
+    error:[false, undefined],
+    loading:false
+  })
   return (
     <main className="min-h-screen flex">
-      <AuthBanner>
-        <form className="" onSubmit={(e)=>{
-          e.preventDefault()
-          console.log(state);
-          axios.post('https://link-a-share.herokuapp.com/signup.php',state)
-          .then(res=>{
-            console.log(res)
-          })
-          .catch(err=>{
-            console.error(err)
-          })
-        }}>
-          <Input
-            type="text"
-            label="Username"
-            placeholder="johnDoe"
-            onChange={(e:any)=> setState({...state,username:e.target.value})}
-            icon={<FaUser />}
-          />
+    <AuthBanner>
+      <form className="" onSubmit={(e)=>{
+        e.preventDefault()
+        console.log(state);
+      
+       
+      }}>
 
-          <Input
-            type="email"
-            label="E-mail"
-            placeholder="example@mail.com"
-             onChange={(e:any)=> setState({...state,email:e.target.value})}
-            icon={<FaEnvelope />}
-          />
+        <Input
+          type="text"
+          label="Username"
+          placeholder="johnDoe"
+           onChange={(e:any)=> setState({...state,username:e.target.value})}
+          icon={<FaUser />}
+        />
+        <Input
+          type="email"
+          label="E-mail"
+          placeholder="example@mail.com"
+           onChange={(e:any)=> setState({...state,email:e.target.value})}
+          icon={<FaEnvelope />}
+        />
 
-          <Input
-            type="password"
-            label="Password"
-            placeholder="*******"
-             onChange={(e:any)=> setState({...state,password:e.target.value})}
-            icon={<FaLock />}
-          />
+        <Input
+          type="password"
+          label="Password"
+          placeholder="*******"
+           onChange={(e:any)=> setState({...state,password:e.target.value})}
+          icon={<FaLock />}
+        />
 
-          <Link
-            className="underline hover:decoration-double text-primary"
-            to="/forgotten-password"
-          >
-            Forgotten Password?
-          </Link>
-          <div>
-            <button className="p-5 py-4 text-white hover:bg-dark transition-colors my-6 w-full bg-primary">
-              Create an Account
-            </button>
-          </div>
-        </form>
-      </AuthBanner>
-    </main>
+        <Link
+          className="underline hover:decoration-double text-primary"
+          to="/forgotten-password"
+        >
+          Forgotten Password?
+        </Link>
+        <div>
+          <button disabled={progress.loading} className={`p-5 py-4 text-white hover:bg-dark transition-colors my-6 cursor-pointer w-full bg-primary disabled:pointer-events-none disabled:opacity-60 disabled:select-none`}>
+            {progress.loading ?(
+              <div className="">
+                Please Wait ...
+              </div>
+            ): 'Create an Account'}
+          </button>
+        </div>
+      </form>
+    </AuthBanner>
+  </main>
   );
 };
 
