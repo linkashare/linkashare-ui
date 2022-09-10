6215bd588d522fff0c0cecbf378f8fad81b375f3
favourite1975 <godsfavour1975.com>
2022-09-10 01:44:30 +0100

    finally done and completed just remaining to fix error

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -133,39 +133,41 @@ const HandleFavourite=(_data:any)=>{
             <div>
                 <DashboardNavbar/>
             </div>
-            <button className='bg-primary  lg:m-12 hover:text-dark hover:bg-white  cursor-pointer lg:w-[100px] lg:h-[100px] w-[50px] h-[50px] flex items-center justify-center text-xl fixed lg:bottom-0 lg:right-0 bottom-2 right-[50%] rounded-full  ease-linear transition-all duration-150' onClick={() => setShowModal(true)}><FaPlus /></button>
-        <div className="lg:px-[4rem] px-[1rem]">
-            <div className='flex absolute w-[100px] h-[100px] lg:w-[400px] lg:h-[400px] left-[249px] top-[-50px] bg-[#4246FF] blur-[400px] bg-blend-darken'></div>
+            <div className="flex justify-center items-center">
+                <button className='bg-primary  lg:m-12 hover:text-dark hover:bg-white  cursor-pointer lg:w-[100px] lg:h-[100px] w-[50px] h-[50px] flex items-center justify-center text-xl fixed lg:bottom-0 lg:right-0 bottom-2 rounded-full  ease-linear transition-all duration-150' onClick={() => setShowModal(true)}><FaPlus /></button>
+            </div>
+            <div className="lg:px-[4rem] px-[1.5rem]">
+            <div className='flex absolute w-[100px] h-[100px] lg:w-[400px] lg:h-[400px] left-[249px] top-[-50px] bg-[#4246FF] -z-50 blur-[400px] bg-blend-darken'></div>
 
-            <div className="capitalize text-[30px] pt-[80px] font-bold">
+            <div className="capitalize lg:text-[30px] text-[25px] pt-[80px] font-bold">
                 welcome, <span className="font-gotham text-primarycolor">{userInfo.username || 'User'}</span>
             </div>
 
-            <div className="w-full flex mt-8 justify-between ">
+            <div className="w-full flex mt-8 justify-between flex-col lg:flex-row">
 
-                <div className="flex lg:flex-row lg:items-center lg:w-[400px] lg:h-[189px] bg-secondry rounded-2xl lg:pl-[2rem] pt-2 cursor-pointer">
+                <div className="flex lg:flex-row items-center pl-5 h-[130px] lg:w-[400px] lg:h-[189px] bg-secondry rounded-2xl lg:pl-[2rem] pt-2 cursor-pointer">
                     <div>
-                        <img src={linkimg} alt="" />
+                        <img src={linkimg} alt="" className="w-[80px] lg:w-max"/>
                     </div>
-                    <div>
+                    <div className='pl-3 lg:pl-0'>
                         <div className="lg:pl-4 lg:text-[25px] lg:text-left text-xl text-center ">Total Links</div>
-                        <div className="lg:pl-4 text-[40px] lg:text-left text-center text-primary">{links.length || '0'}</div>
+                        <div className="lg:pl-4 text-[40px] lg:text-left  text-primary">{links.length || '0'}</div>
                     </div>
                 </div>
 
-                <div className="flex lg:flex-row lg:items-center lg:w-[400px] lg:h-[189px] bg-secondry rounded-2xl lg:pl-[2rem] pt-2 cursor-pointer">
+                <div className="flex lg:flex-row items-center pl-5 h-[130px] lg:w-[400px] lg:h-[189px] bg-secondry rounded-2xl lg:pl-[2rem] pt-2 cursor-pointer my-3 lg:my-0">
                     <div>
-                        <img src={star} alt="" />
+                        <img src={star} alt="" className="w-[80px] lg:w-max"/>
                     </div>
                     <div>
                         <div className="lg:pl-4 lg:text-[25px] lg:text-left text-xl text-center">Starred Links</div>
-                        <div className="lg:pl-4 text-[40px] lg:text-left text-center text-primary">{favouritesNo || '0'}</div>
+                        <div className="lg:pl-4 text-[40px] lg:text-left text-primary">{favouritesNo || '0'}</div>
                     </div>
                 </div>
 
-                <div className="flex lg:flex-row lg:items-center lg:w-[400px] lg:h-[189px] bg-secondry rounded-2xl lg:pl-[2rem] pt-2 cursor-pointer">
+                <div className="flex lg:flex-row items-center pl-5 h-[130px] lg:w-[400px] lg:h-[189px] bg-secondry rounded-2xl lg:pl-[2rem] pt-2 cursor-pointer">
                     <div>
-                        <img src={add} alt="" />
+                        <img src={add} alt="" className="w-[80px] lg:w-max"/>
                     </div>
                     <div>
                         <a href={links.length > 0? links[links.length-1].fullurl : ''} target="_blank">
@@ -179,182 +181,175 @@ const HandleFavourite=(_data:any)=>{
                     </div>
                 </div>
             </div>
-            <div className="py-3 pl-6 lg:text-[25px] text-[20px] text-primarycolor font-bold">
+            <div className="py-5 pl-6 lg:text-[25px] text-[20px] text-primarycolor font-bold">
             Keep your links in sync
             </div>
     
-    <div className="flex flex-row justify-between text-4xl font-bold px-8 py-3">
-    <div>Favourites </div>
-
-    </div>
-    
-    {showModal ? (
-    <>
-        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-        <div className="relative  w-[75vw] my-6 mx-auto max-w-3xl">
-            {/*content*/}
-            <div className="border-0 rounded-lg  shadow-lg relative flex flex-col bg-dark outline-none focus:outline-none">
-            <form
-                className="p-12"
-                autoComplete="false"
-                onSubmit={(e) => {
-                e.preventDefault();
-                }}
-            >
-                <Input
-                type="text"
-                label="Title"
-                placeholder="My Portfolio"
-                onChange={(e: any) =>
-                    setLink({ ...link, title: e.target.value })
-                }
-                />
-                <Input
-                type="text"
-                label="Url"
-                placeholder="Enter Url"
-                onChange={(e: any) =>
-                    setLink({ ...link, fullurl: e.target.value })
-                }
-                />
-                <Input
-                type="text"
-                label="Category"
-                placeholder="Enter A Category"
-                onChange={(e: any) =>
-                    setLink({ ...link, category: e.target.value })
-                }
-                />
-                <div>
-                <button
-                    className={`p-0 py-4 hover:text-dark hover:bg-white my-1 cursor-pointer w-full bg-primary disabled:pointer-events-none rounded-lg disabled:opacity-60 disabled:select-none ease-linear transition-all duration-150`}
-                    onClick={handleModal}
-                >
-                    {" "}
-                    submit{" "}
-                </button>
-                <button
-                    className={`p-0 py-4 hover:text-dark hover:bg-white my-1 cursor-pointer w-full bg-[red] disabled:pointer-events-none rounded-lg disabled:opacity-60 disabled:select-none ease-linear transition-all duration-150`}
-                    onClick={() => setShowModal(false)}
-                >
-                    {" "}
-                    close{" "}
-                </button>
+            {showModal ? (
+            <>
+                <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                <div className="relative  w-[75vw] my-6 mx-auto max-w-3xl">
+                    {/*content*/}
+                    <div className="border-0 rounded-lg  shadow-lg relative flex flex-col bg-dark outline-none focus:outline-none">
+                    <form
+                        className="p-12"
+                        autoComplete="false"
+                        onSubmit={(e) => {
+                        e.preventDefault();
+                        }}
+                    >
+                        <Input
+                        type="text"
+                        label="Title"
+                        placeholder="My Portfolio"
+                        onChange={(e: any) =>
+                            setLink({ ...link, title: e.target.value })
+                        }
+                        />
+                        <Input
+                        type="text"
+                        label="Url"
+                        placeholder="Enter Url"
+                        onChange={(e: any) =>
+                            setLink({ ...link, fullurl: e.target.value })
+                        }
+                        />
+                        <Input
+                        type="text"
+                        label="Category"
+                        placeholder="Enter A Category"
+                        onChange={(e: any) =>
+                            setLink({ ...link, category: e.target.value })
+                        }
+                        />
+                        <div>
+                        <button
+                            className={`p-0 py-4 hover:text-dark hover:bg-white my-1 cursor-pointer w-full bg-primary disabled:pointer-events-none rounded-lg disabled:opacity-60 disabled:select-none ease-linear transition-all duration-150`}
+                            onClick={handleModal}
+                        >
+                            {" "}
+                            submit{" "}
+                        </button>
+                        <button
+                            className={`p-0 py-4 hover:text-dark hover:bg-white my-1 cursor-pointer w-full bg-[red] disabled:pointer-events-none rounded-lg disabled:opacity-60 disabled:select-none ease-linear transition-all duration-150`}
+                            onClick={() => setShowModal(false)}
+                        >
+                            {" "}
+                            close{" "}
+                        </button>
+                        </div>
+                    </form>
+                    </div>
                 </div>
-            </form>
+                </div>
+                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+            </>
+            ) : null}
+    <div className="flex lg:flex-row flex-col w-full lg:justify-between pb-4">
+        <div className="bg-secondry lg:px-12 px-3 rounded-lg lg:w-1/2">
+
+            <div className="font-bold text-[25px] pt-5">Favourites </div>
+
+            <div className="w-full pb-5">
+                {
+                    links.length > 0?(
+                        <div className="">
+                        {links
+                            .slice(-4)
+                            .map(
+                            (data: {
+                                title: string;
+                                isFavourite:string,
+                                fullurl: string | undefined;
+                                timeAdded: string;
+                            }) => {
+                            return data.isFavourite == 'true'? (
+                                <div key={data.timeAdded}>
+                                <div
+                                    className="py-6 px-8 my-3 flex flex-auto justify-between bg-secondrycolor rounded-2xl"
+                                >
+                                    <div>
+                                    <div className="text-primary pb-3 text-[25px] font-bold">
+                                    <a href={data.fullurl}>
+                                    {data.title}
+                                    </a>
+                                    </div>
+                                    <div className="text-sm">{data.timeAdded}</div>
+                                    </div>
+                        
+                                <div className="flex items-center gap-3">
+                                <div onClick={()=> HandleFavourite(data) } className='text-3xl cursor-pointer'>
+                                        { data.isFavourite == 'true' ? <AiFillStar />: <AiOutlineStar />}
+                                    </div>
+                                    <div onClick={()=> deleteLink(data.title) } className='text-xl text-red-400 cursor-pointer'>
+                                        <FaTrash />
+                                    </div>
+                                </div>
+                                </div>
+                                </div>
+                            ):<span></span>
+                            
+                            }
+                            )}
+                        </div>
+                    ):(<h2 className='lg:text-[25px] text-[20px] capitalize py-4 text-[#79839D] flex justify-center items-center'>No links added yet</h2>)
+                }
             </div>
         </div>
-        </div>
-        <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-    </>
-    ) : null}
-    
-    <div className="w-full lg:px-14 sm:px-8 pb-5">
-  {
-      links.length > 0?(
-        <div className="">
-        {links
-            .slice(-4)
-             .map(
-             (data: {
-                 title: string;
-                 isFavourite:string,
-                 fullurl: string | undefined;
-                 timeAdded: string;
-             }) => {
-               return data.isFavourite == 'true'? (
-                <div key={data.timeAdded}>
-                <div
-                    className="py-6 px-8 my-3 flex flex-auto justify-between bg-[#1F1F1F] rounded-2xl"
-                >
-                    <div>
-                    <div className="text-primary pb-3 text-[25px] font-bold">
-                       <a href={data.fullurl}>
-                       {data.title}
-                       </a>
-                    </div>
-                    <div className="text-sm">{data.timeAdded}</div>
-                    </div>
         
-                   <div className="flex items-center gap-3">
-                   <div onClick={()=> HandleFavourite(data) } className='text-3xl cursor-pointer'>
-                        { data.isFavourite == 'true' ? <AiFillStar />: <AiOutlineStar />}
-                    </div>
-                    <div onClick={()=> deleteLink(data.title) } className='text-xl text-red-400 cursor-pointer'>
-                        <FaTrash />
-                    </div>
-                   </div>
-                </div>
-                </div>
-               ):<span></span>
-               
-             }
-             )}
-        </div>
-      ):(<h2 className='text-2xl py-4'>No links added yet</h2>)
-  }
-    </div>
-    
-    <div className="flex flex-row justify-between text-4xl font-bold px-8 py-3">
-    <div>All Links</div>
-    {/* <div>
-        <button
-        className="text-[30px] hover:text-primary ease-linear transition-all duration-150"
-        onClick={() => setShowModal(true)}
-        >
-        +
-        </button>
-    </div> */}
-    </div>
-    
-    <div className="w-full lg:px-14 sm:px-8 pb-5">
-  {
-      links.length > 0?(
-        <div className="">
-        {links
-            
-             .map(
-             (data: {
-                 title: string;
-                 isFavourite:string,
-                 fullurl: string | undefined;
-                 timeAdded: string | number | boolean;
-             }) => {
-                 return (
-                     <div key={data.title + data.fullurl}>
-                     <div
-                         className="py-6 px-8 my-3 flex flex-auto justify-between bg-secondry rounded-2xl"
-                     >
-                         <div>
-                         <div className="text-primary pb-3 text-[25px] font-bold">
-                            <a href={data.fullurl}>
-                                <div>
-                                    {data.title}
-                                </div>
-                            </a>
-                         </div>
-                         <div className="text-sm">{data.timeAdded}</div>
-                         </div>
-             
-                        <div className="flex items-center gap-3 lg:pr-14">
-                        <div onClick={()=> HandleFavourite(data) } className='text-3xl cursor-pointer'>
-                             { data.isFavourite == 'true' ? <AiFillStar />: <AiOutlineStar />}
-                         </div>
-                         <div onClick={()=> deleteLink(data.title) } className='text-xl text-red-400 cursor-pointer'>
-                             <FaTrash />
-                         </div>
-                        </div>
-                     </div>
-                     </div>
-                 );
-             }
-             )}
-        </div>
-      ):(<h2 className='text-2xl py-4'>No links added yet</h2>)
-  }
-    </div>
-    </div>
+        <div className="lg:ml-8 bg-secondry lg:px-12 px-3 rounded-lg lg:w-1/2 mt-3 lg:mt-0">
 
+            <div className="font-bold text-[25px] pt-5">All Links </div>
+
+        <div className="w-full pb-5">
+            {
+                links.length > 0?(
+                    <div className="">
+                    {links
+                        
+                        .map(
+                        (data: {
+                            title: string;
+                            isFavourite:string,
+                            fullurl: string | undefined;
+                            timeAdded: string;
+                        }) => {
+                            return (
+                                <div key={data.timeAdded}>
+                                <div
+                                    className="py-6 px-8 my-3 flex flex-auto justify-between bg-secondry rounded-2xl"
+                                >
+                                    <div>
+                                    <div className="text-primary pb-3 text-[25px] font-bold">
+                                        <a href={data.fullurl} target="_blank">
+                                            <div>
+                                                {data.title}
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div className="text-sm">{data.timeAdded}</div>
+                                    </div>
+                        
+                                    <div className="flex items-center gap-3 lg:pr-14">
+                                    <div onClick={()=> HandleFavourite(data) } className='text-3xl cursor-pointer'>
+                                        { data.isFavourite == 'true' ? <AiFillStar />: <AiOutlineStar />}
+                                    </div>
+                                    <div onClick={()=> deleteLink(data.title) } className='text-xl text-red-400 cursor-pointer'>
+                                        <FaTrash />
+                                    </div>
+                                    </div>
+                                </div>
+                                </div>
+                            );
+                        }
+                        )}
+                    </div>
+                ):(<h2 className='lg:text-[25px] text-[20px] capitalize py-4 text-[#79839D] flex justify-center items-center'>No links added yet</h2>)
+            }
+        </div>
+        </div>
+    </div>
+</div>
             </>
         )
     }
