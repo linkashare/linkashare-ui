b85dd45ca4fbe07f703792f502e690675dfe6a9a
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-09-10 17:23:03 +0100

    fix: homepage design

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -67,46 +67,52 @@ const Home = () => {
           <header>
             <Navbar />
           </header>
-          <div className="flex bg-dark text-textColor h-screen w-full flex-col">
-            <div className='hidden lg:flex absolute w-[400px] h-[400px] left-[249px] top-[-200px] bg-[#4246FF] blur-[400px] bg-blend-darken'></div>
-            <div className='relative'>
-              <img src={rocket} alt="" className='w-[10rem] h-[10rem] lg:w-[10rem] lg:h-[10rem] absolute top-10 right-10 opacity-5 sm:w-[8rem] sm:h-[8rem] sm:top-14 sm:right-0'/>
-            </div>
-            <div className="flex lg:flex-row flex-col w-full pt-[8rem] justify-center items-center">
-              <div className='w-1/2 pl-[3rem] hidden lg:flex'>
-                <img src={linklogo} alt="" className='w-[550px] pl-[5rem] '/>
-              </div>
-              <div className='lg:w-1/2 w-full justify-center items-center lg:px-[0] px-4'>
-                <div className='lg:text-[70px] text-[50px] font-gotham lg:text-left text-center pt-[20px] lg:pt-0'>Linkashare</div>
-                <div className='lg:text-[25px] capitalize text-[1.5rem] py-[10px] lg:px-0 px-2 font-bold lg:text-left text-center'>Edit links . <span className='text-primarycolor'>keep links in sync</span> . save links</div>
-                <div className='w-full flex lg:flex-row md:flex-row pt-4 sm:flex-col lg:justify-start justify-center items-center'>
-                <input type="text"
-                      className='lg:w-[50%] h-[3rem] rounded-lg outline-none text-dark pl-4 w-[80%]'
-                      required 
-                      placeholder="Enter a url"
-                      onChange={(e:any)=> setState({...state,url:e.target.value})}
+        
 
-                />
-                <button className='bg-primary md:mt-0 lg:mt-0 ml-3 px-4 py-3 font-bold rounded-full sm:mt-3 sm:px-8 z-50' type="submit"  onClick={handleSubmit}>submit</button>
-                
-                </div>
-                {showText && (<div className='pt-3 text-center lg:text-left'>Your Shortened Link is: {progress.loading ?(
-              <span className="">
-                Please Wait ...
-              </span>
-            ):<span><a href={newUrl} target="_blank" className='cursor-pointer hover:text-primarycolor'>{newUrl}</a></span>}</div>)}
-                  </div>
-            </div>
+        <div className=" flex flex-col-reverse md:flex-row bg-dark justify-around items-center text-textColor  py-[15%]">
+          {/* left side */}
+      <div className=''>
+    <img src={linklogo} alt="" className="max-w-[400px]"/>
+      </div>
 
-            <div className='hidden lg:flex absolute w-[300px] rounded-2xl h-[300px] left-[1000px] top-[299px] bg-[#42A4FF] blur-[804px] bg-blend-darken'></div>
-          </div>
-          <ToastContainer />
-          <div className='bg-[#000] fixed bottom-0 w-full px-2 lg:px-[6rem] py-2 flex flex-row justify-between items-center text-textColor'>
-            <div className='lg:text-[30px] text-[20px] py-1 font-bold text-primary flex justify-center items-center'>Buy Us A Coffee <span className='lg:pl-2 text-textColor'><FaCoffee /></span></div>
-            <div>
-              <button className='lg:py-3 lg:px-8 py-2 px-3 bg-primary font-bold mt-2 rounded-lg text-[#000]'>Support</button>
-            </div>
-          </div>
+
+    {/* right side  */}
+
+    <div className="px-5">
+   <div className=" text-center md:text-left py-3">
+      <h1 className="font-bold text-6xl py-4">Linkashare</h1>
+     <p className='capitalize text-xl font-bold py-2'>Edit links . <span className='text-primarycolor'>keep links in sync</span> . save links</p> 
+   </div>
+
+    <div className='w-full flex lg:flex-row md:flex-row pt-4 sm:flex-col lg:justify-start justify-center items-center'>
+    <input type="text"
+          className='lg:w-[50%] h-[3rem] rounded-lg outline-none text-dark pl-4 w-[80%]'
+          required 
+          placeholder="Enter a url"
+          onChange={(e:any)=> setState({...state,url:e.target.value})}
+
+    />
+    <button className='bg-primary md:mt-0 lg:mt-0 ml-3 px-4 py-3 font-bold rounded-full sm:mt-3 sm:px-8 z-50' type="submit"  onClick={handleSubmit}>submit</button>
+    
+    </div> 
+     {showText && (<div className='pt-3 text-center lg:text-left'>Your Shortened Link is: {progress.loading ?(
+  <span className="">
+    Please Wait ...
+  </span>
+):<span><a href={newUrl} target="_blank" className='cursor-pointer hover:text-primarycolor'>{newUrl}</a></span>}</div>)}
+
+<ToastContainer />
+    </div>
+
+
+
+        </div>
+
+        <div className='bg-[#000]  w-full px-2  py-4 lg:px-[6rem] flex flex-row justify-between items-center text-textColor'>
+<div className='lg:text-[30px] text-[20px] py-1 px-3 font-bold text-primary flex justify-center items-center'>Buy Us A Coffee <span className='lg:pl-2 text-textColor'><FaCoffee /></span></div>
+
+  <button className='lg:py-3 lg:px-8 py-2 px-3 mx-3 bg-primary font-bold mt-2 rounded-lg text-[#000]'>Support</button>
+</div>
 
         </main>
     </Fragment>
